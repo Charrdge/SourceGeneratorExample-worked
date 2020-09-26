73ad3d39e46ada5781f849bed512c64d38546a5f
Charrdge <skillstree.cs@gmail.com>
2020-09-27 01:48:12 +0300

    Добавьте файлы проекта.

---
+++ Modular.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Modular.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Mod.Cls.SayHello();
+        }
+    }
+}
---
+++ Modular/ModularGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace Modular
+{
+    [Generator]
+    public class ModularGenerator : ISourceGenerator
+    {
+        private const string SOURCE = @"
+using System;
+
+namespace Mod
+{
+    public static class Cls
+    {
+        public static void SayHello() => Console.WriteLine(""Fuck You"");
+    }
+}";
+
+        public void Execute(GeneratorExecutionContext context) => context.AddSource("mod", Microsoft.CodeAnalysis.Text.SourceText.From(SOURCE, Encoding.UTF8));
+
+        public void Initialize(GeneratorInitializationContext context) { }
+    }
+}
